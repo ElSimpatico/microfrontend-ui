8ace2f4deec6cd87c74f612caa1e3c1b1f58c0e2
Aaron Velasco Lopez <tiquismiquisss@gmail.com>
2022-04-24 14:35:20 +0200

    feat: initial commit

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-    namespace: 'stencil-components',
+    namespace: 'microfrontend-components',
     srcDir: 'src',
     taskQueue: 'async',
     outputTargets: [
