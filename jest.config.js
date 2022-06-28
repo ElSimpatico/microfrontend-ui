f6cd8c32f08f315aad57cb260d451e3a7a7116e1
avelascl <avelascl@nttdata.com>
2022-06-28 16:16:13 +0200

    test(jest config): assets module

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,10 @@ module.exports = {
     collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.stories.tsx'],
     moduleNameMapper: {
         ...preset.moduleNameMapper,
+
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/jest-assets-module.js',
+
         '@utils(.*)': '<rootDir>/src/utils$1',
     },
 };
